58e4be302950df3afbb3f816b5d061908312abdd
Justchidi26 <chidiaguwa26@gmail.com>
2025-08-04 20:47:32 +0100

    Updated calculator.c

--- calculator.c
+++ calculator.c
@@ -1,41 +1,50 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello!\n");
-
-    int a;
+    double a, b, answer;
     char c;
-    int b;
-    int answer;
+    int validInput = 0;
 
-    printf("Enter a number: ");
-    scanf("%d", &a);
+    while (!validInput) {
+        printf("Enter a number: ");
+        if (scanf("%lf", &a) != 1) {
+            printf("Error: Invalid input. Please enter a number.\n");
+            while (getchar() != '\n');
+        } else {
+            validInput = 1;
+        }
+    }
 
-    while (getchar() != '\n');
 
     printf("Please enter an operator (+, -, *, /): ");
-    scanf("%c", &c);
+    scanf(" %c", &c);
 
-    printf("Enter another number: ");
-    scanf("%d", &b);
+    validInput = 0;
+    while (!validInput) {
+        printf("Enter another number: ");
+        if (scanf("%lf", &b) != 1) {
+            printf("Error: Invalid input. Please enter a number.\n");
+            while (getchar() != '\n');
+        } else {
+            validInput = 1;
+        }
+    }
+
 
     if (c == '+') {
         answer = a + b;
-        printf("Answer is %d\n", answer);
     } else if (c == '-') {
         answer = a - b;
-        printf("Answer is %d\n", answer);
     } else if (c == '*') {
         answer = a * b;
-        printf("Answer is %d\n", answer);
     } else if (c == '/') {
         if (b == 0) {
-            printf("Invalid: Division by zero\n");
+            printf("Invalid. Division by 0 is not allowed.\n");
         } else {
             answer = a / b;
-            printf("Answer is %d\n", answer);
         }
     } else {
-        printf("Invalid operator\n");
+        printf("Invalid operator.\n");
     }
+    printf("Answer is: %.lf\n", answer);
 }
